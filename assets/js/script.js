3f947405c07fe432bccb80354ca8a95bdebf3513
Faris Shamsudeen <faris52342345@gmail.com>
2025-09-18 11:59:45 +0530

    modified my offers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// preloader variables
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  if (preloader) {
+    preloader.classList.add("loaded");
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
 
   // element toggle function
@@ -14,52 +23,52 @@ document.addEventListener("DOMContentLoaded", function() {
     sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
   }
 
-  // testimonials variables
-  const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-  const modalContainer = document.querySelector("[data-modal-container]");
-  const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-  const overlay = document.querySelector("[data-overlay]");
-
-  // modal variable
-  const modalImg = document.querySelector("[data-modal-img]");
-  const modalTitle = document.querySelector("[data-modal-title]");
-  const modalText = document.querySelector("[data-modal-text]");
-
-  // modal toggle function
-  const testimonialsModalFunc = function () {
-    if (modalContainer) {
-      modalContainer.classList.toggle("active");
-    }
-    if (overlay) {
-      overlay.classList.toggle("active");
-    }
-  }
-
-  // add click event to all modal items
-  for (let i = 0; i < testimonialsItem.length; i++) {
-    testimonialsItem[i].addEventListener("click", function () {
-      if (modalImg) {
-        modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-        modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-      }
-      if (modalTitle) {
-        modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-      }
-      if (modalText) {
-        modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-      }
-      testimonialsModalFunc();
-    });
-  }
-
-  // add click event to modal close button
-  if (modalCloseBtn) {
-    modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-  }
-  if (overlay) {
-    overlay.addEventListener("click", testimonialsModalFunc);
-  }
-
+  // // testimonials variables
+  // const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+  // const modalContainer = document.querySelector("[data-modal-container]");
+  // const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+  // const overlay = document.querySelector("[data-overlay]");
+  //
+  // // modal variable
+  // const modalImg = document.querySelector("[data-modal-img]");
+  // const modalTitle = document.querySelector("[data-modal-title]");
+  // const modalText = document.querySelector("[data-modal-text]");
+  //
+  // // modal toggle function
+  // const testimonialsModalFunc = function () {
+  //   if (modalContainer) {
+  //     modalContainer.classList.toggle("active");
+  //   }
+  //   if (overlay) {
+  //     overlay.classList.toggle("active");
+  //   }
+  // }
+  //
+  // // add click event to all modal items
+  // for (let i = 0; i < testimonialsItem.length; i++) {
+  //   testimonialsItem[i].addEventListener("click", function () {
+  //     if (modalImg) {
+  //       modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+  //       modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+  //     }
+  //     if (modalTitle) {
+  //       modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+  //     }
+  //     if (modalText) {
+  //       modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+  //     }
+  //     testimonialsModalFunc();
+  //   });
+  // }
+  //
+  // // add click event to modal close button
+  // if (modalCloseBtn) {
+  //   modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+  // }
+  // if (overlay) {
+  //   overlay.addEventListener("click", testimonialsModalFunc);
+  // }
+  //
   // custom select variables
   const select = document.querySelector("[data-select]");
   const selectItems = document.querySelectorAll("[data-select-item]");
@@ -142,7 +151,7 @@ document.addEventListener("DOMContentLoaded", function() {
     navigationLinks[i].addEventListener("click", function () {
 
       for (let i = 0; i < pages.length; i++) {
-        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        if (this.querySelector('span').innerHTML.toLowerCase() === pages[i].dataset.page) {
           pages[i].classList.add("active");
           navigationLinks[i].classList.add("active");
           window.scrollTo(0, 0);
@@ -154,4 +163,119 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
   }
+
+  // portfolio modal variables
+  const projectLinks = document.querySelectorAll(".project-link");
+
+  // modal toggle function
+  const portfolioModalFunc = function () {
+    modalContainer.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+
+  // add click event to all project links
+  for (let i = 0; i < projectLinks.length; i++) {
+    projectLinks[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      const projectId = this.dataset.projectId;
+      const projectItem = this.parentElement;
+      const projectTitle = projectItem.querySelector(".project-title").innerHTML;
+      const projectCategory = projectItem.querySelector(".project-category").innerHTML;
+      const projectImgSrc = projectItem.querySelector(".project-img img").src;
+      const projectImgAlt = projectItem.querySelector(".project-img img").alt;
+
+      modalTitle.innerHTML = projectTitle;
+      modalText.innerHTML = `<p>${projectCategory}</p>`;
+      modalImg.src = projectImgSrc;
+      modalImg.alt = projectImgAlt;
+
+      portfolioModalFunc();
+    });
+  }
+
+  // add click event to modal close button
+  if (modalCloseBtn) {
+    modalCloseBtn.addEventListener("click", portfolioModalFunc);
+  }
+  if (overlay) {
+    overlay.addEventListener("click", portfolioModalFunc);
+  }
+
+  // blog modal variables
+  const blogPostLinks = document.querySelectorAll(".blog-post-link");
+  const blogModalContainer = document.querySelector("[data-blog-modal-container]");
+  const blogModalCloseBtn = document.querySelector("[data-blog-modal-close-btn]");
+  const blogOverlay = document.querySelector("[data-blog-overlay]");
+
+  // blog modal variables
+  const blogModalImg = document.querySelector("[data-blog-modal-img]");
+  const blogModalTitle = document.querySelector("[data-blog-modal-title]");
+  const blogModalTime = document.querySelector("[data-blog-modal-time]");
+  const blogModalText = document.querySelector("[data-blog-modal-text]");
+
+  // blog modal toggle function
+  const blogModalFunc = function () {
+    if (blogModalContainer) {
+      blogModalContainer.classList.toggle("active");
+    }
+    if (blogOverlay) {
+      blogOverlay.classList.toggle("active");
+    }
+  }
+
+  // add click event to all blog post links
+  for (let i = 0; i < blogPostLinks.length; i++) {
+    blogPostLinks[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      const blogPostId = this.dataset.blogPostId;
+      const blogPostItem = this.parentElement;
+      const blogPostTitle = blogPostItem.querySelector(".blog-item-title").innerHTML;
+      const blogPostTime = blogPostItem.querySelector("time").innerHTML;
+      const blogPostText = blogPostItem.querySelector(".blog-text").innerHTML;
+      const blogPostImgSrc = blogPostItem.querySelector(".blog-banner-box img").src;
+      const blogPostImgAlt = blogPostItem.querySelector(".blog-banner-box img").alt;
+
+      if (blogModalTitle) {
+        blogModalTitle.innerHTML = blogPostTitle;
+      }
+      if (blogModalTime) {
+        blogModalTime.innerHTML = blogPostTime;
+      }
+      if (blogModalText) {
+        blogModalText.innerHTML = `<p>${blogPostText}</p>`;
+      }
+      if (blogModalImg) {
+        blogModalImg.src = blogPostImgSrc;
+        blogModalImg.alt = blogPostImgAlt;
+      }
+
+      blogModalFunc();
+    });
+  }
+
+  // add click event to blog modal close button
+  if (blogModalCloseBtn) {
+    blogModalCloseBtn.addEventListener("click", blogModalFunc);
+  }
+  if (blogOverlay) {
+    blogOverlay.addEventListener("click", blogModalFunc);
+  }
+
+  // back to top button
+  const backTopBtn = document.querySelector("[data-back-to-top]");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+      backTopBtn.classList.add("active");
+    } else {
+      backTopBtn.classList.remove("active");
+    }
+  });
+
+  if(backTopBtn) {
+    backTopBtn.addEventListener("click", function() {
+      window.scrollTo(0,0);
+    });
+  }
+
 });
